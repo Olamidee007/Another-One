ec4210ab6d631a00e6ddec36466775f1ab6fa022
Olamidee007 <lamide.dammy@gmail.com>
2022-05-07 03:40:15 +0100

    git push origin main

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const main= document.getElementById('id')
+document.getElementById('oA').addEventListener("click", function(){
+  alert('Welcome')
+})
+
+
+document.getElementById('oH').addEventListener("mousemove", function(){
+  alert('You Good?')
+})
+
+document.getElementById('oG').addEventListener("mousedown", function(){
+  alert('Let Me Go')
+})
+
+document.getElementById('oB').addEventListener("mouseover", function(){
+  alert('See You Later')
+})
