70a629a257a628b63437cfb1db9261730470714f
Olamidee007 <lamide.dammy@gmail.com>
2022-05-08 14:48:05 +0100

    git push origin main

---
+++ Here We Are/index.js
@@ -0,0 +1,4 @@
+const p1 = document.getElementById('oA')
+p1.addEventListener('click', function changeText() {
+  p1.textContent= 'Welcome'
+})
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-document.getElementById('oA').addEventListener("click", function(){
-  alert('Welcome')
+// Write a function to change text
+
+function changeText(main,words){
+  main.textContent = words;
+}
+
+document.getElementById('oA').addEventListener("click", function(e){
+  changeText(e.target, 'Welcome')
 })
 
-
-document.getElementById('oH').addEventListener("mousemove", function(){
-  alert('You Good?')
+document.getElementById('oH').addEventListener("mousemove", function(e){
+  changeText(e.target,'You Good?')
+})
+document.getElementById('oH').addEventListener("mouseout", function(e){
+  changeText(e.target,'Mouse On Me')
 })
 
-document.getElementById('oG').addEventListener("mousedown", function(){
-  alert('Let Me Go')
+document.getElementById('oG').addEventListener("mousedown", function(e){
+  changeText(e.target,'Let Me Go')
+})
+document.getElementById('oG').addEventListener("mouseup", function(e){
+  changeText(e.target,'Mouse Down')
 })
 
-document.getElementById('oB').addEventListener("mouseover", function(){
-  alert('See You Later')
+document.getElementById('oB').addEventListener("mouseover", function(e){
+  changeText(e.target,'See You Later')
+})
+document.getElementById('oB').addEventListener("mouseout", function(e){
+  changeText(e.target,'Mouse Over Me')
 })
